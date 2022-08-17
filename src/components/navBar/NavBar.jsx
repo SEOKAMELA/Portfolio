e9698aaf9570baf8e54e04c2f138e7867c09ab16
seokamela <ntjobokoane@gmail.com>
2022-08-18 00:49:45 +0200

    first push

---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/#"><h1>
+                Seokamela</h1>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    {/* <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/#">Home</a>
+          </li> */}
+                    <li class="nav-item">
+                        <a class="nav-link" href="/#">About Me</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/#">Education</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/#">Experince</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/#">Projects</a>
+                    </li>
+                    <button class='hi-button'>
+                        Send Hi
+                    </button>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+    );
+}
+
+export default NavBar;
