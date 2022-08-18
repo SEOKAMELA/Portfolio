c175330c2d373426894023ba9bcd45c9dabd6e09
seokamela <ntjobokoane@gmail.com>
2022-08-18 16:40:12 +0200

    Logo

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from '../../assets/logo3.png'
 
 const NavBar = () => {
     return (<nav class="navbar navbar-expand-lg navbar-light bg-primary">
         <div class="container-fluid">
             <a class="navbar-brand" href="/#">
-                <img src="../../assets/images/logo-2.png" alt="SSS" />
+                <img src={logo}  alt="SSS" />
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
