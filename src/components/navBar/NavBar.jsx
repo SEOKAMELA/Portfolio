9e684193c45f2b5a4e87f4513d6e44bfcbb5a12d
seokamela <ntjobokoane@gmail.com>
2022-08-18 02:28:04 +0200

    Change name

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
     return (<nav class="navbar navbar-expand-lg navbar-light bg-primary">
         <div class="container-fluid">
             <a class="navbar-brand" href="/#"><h1>
-                Seokamela</h1>
+                SeoSSSkamela.</h1>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -13,9 +13,6 @@ const NavBar = () => {
 
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav">
-                    {/* <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/#">Home</a>
-          </li> */}
                     <li class="nav-item">
                         <a class="nav-link" href="/#">About Me</a>
                     </li>
