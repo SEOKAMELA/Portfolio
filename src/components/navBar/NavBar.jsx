d42d950fd1f08e0a5ef119e7076da29099e7dd7f
seokamela <ntjobokoane@gmail.com>
2022-08-24 22:38:54 +0200

    onclick function

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -5,33 +5,33 @@ import './NavBar.css';
 const NavBar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-ligh">
-                <div class="container-fluid">
-                    {/* <a class="navbar-brand" href="/#">
+            <nav className="navbar navbar-expand-lg navbar-light bg-ligh">
+                <div className="container-fluid">
+                    {/* <a className="navbar-brand" href="/#">
                         <img src={logo} alt="Seokamela" id='logo' />
                     </a> */}
                     <img src={logo} alt="Seokamela" id='logo' />
 
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item">
-                                <a id='a'class="nav-link" href="/#">whoami</a>
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <a id='a'className="nav-link" href="/#" >whoami </a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/#">services</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/#">services</a>
                             </li>
-                            {/* <li class="nav-item">
-                                <a class="nav-link" href="/#">uptime</a>
+                            {/* <li className="nav-item">
+                                <a className="nav-link" href="/#">uptime</a>
                             </li> */}
-                            <li class="nav-item">
-                                <a class="nav-link" href="/#">projects</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/#">projects</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/#">post</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/#">post</a>
                             </li>
                         </ul>
                         
--- src/views/screen_two/Screen_two.jsx
+++ src/views/screen_two/Screen_two.jsx
@@ -2,7 +2,26 @@ import React from 'react';
 import image from '../../assets/me.png'
 import './Screen_two.css'
 
+
+
+
+
 const ScreenTwo = () => {
+
+    function openTab() {
+
+    
+
+        // console.log("HEllo world")
+    
+        // var i = 0
+        // if(i === 3) {
+        //     tablinks.classList.remove("active-link")
+    
+        // }
+    
+    }
+    
 
 
     // var tablinks = document.getElementsByClassName("tab-links")
@@ -10,23 +29,42 @@ const ScreenTwo = () => {
 
     // function openTab(tabname) {
 
-    //     for (var tablink of tablinks) {
-    //         tablink.classList.remove("active-link")
+    //     for (var tablinks of tablinks) {
+    //         tablinks.classList.remove("active-link")
     //     }
     //     for (var tancontent of tancontents) {
     //         tancontent.classList.remove("active-tab")
     //     }
+
     // }
 
+
+    // function openTab() {
+    //     // const sign_in_btn = document.querySelector("#sign-in-btn");
+    //     // const sign_up_btn = document.querySelector("#sign-up-btn");
+    //     // const container = document.querySelector(".container");
+    //     // sign_up_btn.addEventListener("click", () => {
+    //     //   container.classList.add("sign-up-mode");
+    //     // });
+
+    //     // sign_in_btn.addEventListener("click", () => {
+    //     //   container.classList.remove("sign-up-mode");
+    //     // });
+    //   }
+
+ 
+
+
     return (
+        
         <div id="about-me">
-            <div class="about-row">
-                <div class="pic-col">
+            <div className="about-row">
+                <div className="pic-col">
                     <img src={image} alt="" />
                 </div>
-                <div class="about-col">
-                    <h1 class='sub-titlle'>whoami</h1>
-                    <p class='title'>
+                <div className="about-col">
+                    <h1 className='sub-titlle'>whoami</h1>
+                    <p className='title'>
                         ~ I am a software Developer who is passionate about
                         learning new technologies. I
                         have excellent
@@ -37,18 +75,18 @@ const ScreenTwo = () => {
                         apply and grow my IT skills
                         while bringing results to the
                         place of employment </p>
-                    <div class="tab">
-                        <p className="tab-links active-link">
+                    <div className="tab-titles">
+                        <p className="tab-links active-link" onClick={ () => alert()}>
                             Skils
                         </p>
-                        <p className="tab-links" >
-                            Experince
+                        <p className="tab-links " onClick={() => openTab()}>
+                            Experience
                         </p>
-                        <p className="tab-links" >
+                        <p className="tab-links" onClick={() =>  openTab()}>
                             Education
                         </p>
                     </div>
-                    <div className="tab-contents active-tab">
+                    <div className="tab-contents active-tab" id='skills'>
                         <ul>
                             <li>
                                 <span>Frontend</span>
@@ -68,7 +106,7 @@ const ScreenTwo = () => {
                             </li>
                         </ul>
                     </div>
-                    <div className="tab-contents">
+                    <div className="tab-contents" id='experience'>
                         <ul>
                             <li>
                                 <span>Web Developer</span>
@@ -77,7 +115,7 @@ const ScreenTwo = () => {
                                 Backend
                             </li>
                         </ul>
-                    </div><div className="tab-contents">
+                    </div><div className="tab-contents" id='education'>
                         <ul>
                             <li>
                                 <span>Wethinkcode_</span>
@@ -97,6 +135,8 @@ const ScreenTwo = () => {
             </div>
         </div>
     );
+
+    
 }
 
 export default ScreenTwo;
