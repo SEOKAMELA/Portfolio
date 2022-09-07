0ab5c223698cc2a05fc5eccf53993baf1de9b815
seokamela <ntjobokoane@gmail.com>
2022-09-07 22:38:46 +0200

    links added to page one, projects added to projects page but need information added

--- src/views/projects_view/Projects_view.jsx
+++ src/views/projects_view/Projects_view.jsx
@@ -13,13 +13,41 @@ const ProjectsView = () => {
                     Projects
                 </h1>
                 <div className="projects-list">
-                    <div className="projects">
+                    <div className="project">
                         <img src={logo} alt="Seokamela" id='logo' />
+                        <div className="layer">
+                            <h3>
+                                Mobile app
+                            </h3>
+                            <p>
+                                This a a smart contract voting system, using flutter in the front end
+                            </p>
+                            <a href='#'><i class="fa-solid fa-up-right-from-square"></i></a>
+                        </div>
                     </div>
-                    <div className="projects">
+                    <div className="project">
                         <img src={logo2} alt="Seokamela" id='logo' />
-                    </div>                    <div className="projects">
+                        <div className="layer">
+                            <h3>
+                                Mobile app
+                            </h3>
+                            <p>
+                                This a a smart contract voting system, using flutter in the front end
+                            </p>
+                            <a href='#'><i class="fa-solid fa-up-right-from-square"></i></a>
+                        </div>
+                    </div>
+                    <div className="project">
                         <img src={logo} alt="Seokamela" id='logo' />
+                        <div className="layer">
+                            <h3>
+                                Mobile app
+                            </h3>
+                            <p>
+                                This a a smart contract voting system, using flutter in the front end
+                            </p>
+                            <a href='#'><i class="fa-solid fa-up-right-from-square"></i></a>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/views/screen_one/Screen_one.jsx
+++ src/views/screen_one/Screen_one.jsx
@@ -13,12 +13,29 @@ const ScreenOne = () => {
                 </p>
                 <h1 className="my-name">
                     I'm <span className='spane-name'>Seokamela</span>
-                    <br/>
+                    <br />
                     David Ntjobokoane
                 </h1>
                 <p>
                     software developer
                 </p>
+                <a className='icons' href='https://www.linkedin.com/in/seokamela-ntjobokoane/'>
+                    <i class="fa-brands fa-linkedin"></i>
+
+                </a>
+
+                <a className='icons' href='https://www.facebook.com/seokamela.ntjobokoane'>
+                    <i class="fa-brands fa-square-facebook"></i>
+
+                </a>
+                <a className='icons' href='https://wa.me/+27786189190'>
+                    <i class="fa-brands fa-square-whatsapp"></i>
+
+                </a>
+                <a className='icons' href='https://twitter.com/seokamela'>
+                    <i class="fa-brands fa-square-twitter"></i>
+
+                </a>
             </div>
         </div>
 
