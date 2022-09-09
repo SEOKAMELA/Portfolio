48083a1b133db6fe988a110b8b275ab926760d5a
seokamela <ntjobokoane@gmail.com>
2022-09-09 22:44:12 +0200

    m

--- src/views/projects_view/Projects_view.jsx
+++ src/views/projects_view/Projects_view.jsx
@@ -1,56 +1,55 @@
 import React from "react";
 import "./Projects_view.css";
 import logo from "../../assets/images/splash.jpeg";
-import logo2 from "../../assets/images/search.jpeg";
+import logo2 from "../../assets/images/snacktime.png";
 
 const ProjectsView = () => {
   return (
     <div id="projects">
-      <div className="container">
-        <h1 className="sub-title">Projects</h1>
-        <div className="projects-list">
-          <div className="project">
-            <img src={logo} alt="Seokamela" id="logo" />
-            <div className="layer">
-              <h4>Mobile app</h4>
-              <p>
-                This a a smart contract voting system, using flutter in the
-                front end
-              </p>
-              <a href="/#">
-                <i class="fa-solid fa-up-right-from-square"></i>
-              </a>
-            </div>
+      {/* <div className="container"> */}
+      <h1 className="sub-title">Projects</h1>
+      <div className="projects-list">
+        <div className="project">
+          <img src={logo} alt="Seokamela" id="logo" />
+          <div className="layer">
+            <h4>Ballot</h4>
+            <p>
+              A smart contract mobile app which allows users to vote for their
+              desired political parties in the comfort of their home.
+            </p>
+            <a href="/#">
+              <i class="fa-solid fa-up-right-from-square"></i>
+            </a>
           </div>
-          <div className="project">
-            <img src={logo2} alt="Seokamela" id="logo" />
-            <div className="layer">
-              <h4>Mobile app</h4>
-              <p>
-                This a a smart contract voting system, using flutter in the
-                front end
-              </p>
-              <a href="/#">
-                <i class="fa-solid fa-up-right-from-square"></i>
-              </a>
-            </div>
+        </div>
+        <div className="project">
+          <img src={logo2} alt="Seokamela" id="logo" />
+          <div className="layer">
+            <h4>SnackTime Web app</h4>
+            <p>
+              A mobile food ordering app. which targets the small business around the locations.
+            </p>
+            <a href="/#">
+              <i class="fa-solid fa-up-right-from-square"></i>
+            </a>
           </div>
-          <div className="project">
-            <img src={logo} alt="Seokamela" id="logo" />
-            <div className="layer">
-              <h4>Mobile app</h4>
-              <p>
-                This a a smart contract voting system, using flutter in the
-                front end
-              </p>
-              <a href="/#">
-                <i class="fa-solid fa-up-right-from-square"></i>
-              </a>
-            </div>
+        </div>
+        <div className="project">
+          <img src={logo} alt="Seokamela" id="logo" />
+          <div className="layer">
+            <h4>Mobile app</h4>
+            <p>
+              This a a smart contract voting system, using flutter in the front
+              end
+            </p>
+            <a href="/#">
+              <i class="fa-solid fa-up-right-from-square"></i>
+            </a>
           </div>
         </div>
       </div>
     </div>
+    // </div>
   );
 };
 
--- src/views/screen_three/Screen_three.jsx
+++ src/views/screen_three/Screen_three.jsx
@@ -1,31 +1,29 @@
-import React from 'react'
-import './Screen_three.css'
+import React from "react";
+import "./Screen_three.css";
 
 const ScreenThree = () => {
   return (
     <div id="services">
-      <div className="container">
-        <h1 className="sub-titlle">My services</h1>
-        <div className="services-list">
-          <div>
-            <h2>Web Development</h2>
-            <p>Javascript HTML CSS</p>
-            <a href="/#">learn more</a>
-          </div>
-          <div>
-            <h2>mobile development</h2>
-            <p>Flutter Dart</p>
-            <a href="/#">learn more</a>
-          </div>
-          <div>
-            <h2>Backend development</h2>
-            <p>Java, Pyhton</p>
-            <a href="/#">learn more</a>
-          </div>
+      <h1 className="sub-titlle">My services</h1>
+      <div className="services-list">
+        <div>
+          <h2>Web Development</h2>
+          <p>Javascript HTML CSS</p>
+          <a href="/#">learn more</a>
+        </div>
+        <div>
+          <h2>mobile development</h2>
+          <p>Flutter Dart</p>
+          <a href="/#">learn more</a>
+        </div>
+        <div>
+          <h2>Backend development</h2>
+          <p>Java, Pyhton</p>
+          <a href="/#">learn more</a>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ScreenThree
+export default ScreenThree;
