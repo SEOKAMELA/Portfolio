0f3ecd19a2c075f947a557cbe8549ccbf7cab920
seokamela <ntjobokoane@gmail.com>
2022-09-21 16:18:55 +0200

    contact resized

--- src/views/contact/Contact.jsx
+++ src/views/contact/Contact.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import './contact.css'
-import cv from '../../assets/documents/Seokamela David Ntjobokoane.pdf'
+import React from "react";
+import "./contact.css";
+import cv from "../../assets/documents/Seokamela David Ntjobokoane.pdf";
 
 const Contact = () => {
   return (
     <div id="conatact">
       <div className="conatact-container">
+        <div className="row">
           <div className="contact-left">
             <h1 className="sub-title">Contact me</h1>
-            <p> <i class="fa-regular fa-envelope"></i> ntjobokoane@gmail.com</p>
-            <p> <i class="fa-solid fa-mobile-screen"></i> +27 78 618 9190</p>
+            <p>
+              {" "}
+              <i class="fa-regular fa-envelope"></i> ntjobokoane@gmail.com
+            </p>
+            <p>
+              {" "}
+              <i class="fa-solid fa-mobile-screen"></i> +27 78 618 9190
+            </p>
             <div className="socila-icons">
-              <a 
+              <a
                 className="icons"
                 href="https://www.linkedin.com/in/seokamela-ntjobokoane"
                 target="_blank"
@@ -46,11 +53,11 @@ const Contact = () => {
                 <i class="fa-brands fa-square-twitter"></i>
               </a>
             </div>
-            <a className='btn-cv btn-cv2' href={cv} download >
+            <a className="btn-cv btn-cv2" href={cv} download>
               Get CV
             </a>
           </div>
-          <div className="contact-right" >
+          <div className="contact-right">
             <form>
               <input
                 type="text"
@@ -66,15 +73,18 @@ const Contact = () => {
               ></input>
               <textarea
                 name="message"
-                rows={6}
+                rows={3}
                 placeholder="Leave a message"
               ></textarea>
-              <button className='btn-post btn-post' typeof="submit">Post</button>
+              <button className="btn-post btn-post" typeof="submit">
+                Post
+              </button>
             </form>
           </div>
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
