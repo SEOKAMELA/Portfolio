f64f7ae86f4a1199b23673cbedfb0a42053b9413
seokamela <ntjobokoane@gmail.com>
2023-03-16 08:10:17 +0200

    removed commented code

--- src/views/projects_view/Projects_view.jsx
+++ src/views/projects_view/Projects_view.jsx
@@ -6,52 +6,7 @@ import logo3 from "../../assets/images/Portfo.png";
 
 const ProjectsView = () => {
   return (
-    // <div id="projects">
-    //   <div className="projects">
-    //     <h1 className="sub-title">Projects</h1>
-    //     <div className="projects-list">
-    //       <div className="project">
-    //         <img src={logo} alt="Seokamela" id="logo" />
-    //         <div className="layer">
-    //           <h4>Ballot</h4>
-    //           <p>
-    //             A smart contract mobile app using Flutter and Rust.
-    //           </p>
-    //           <a href="/#">
-    //             <i class="fa-solid fa-up-right-from-square"></i>
-    //           </a>
-    //         </div>
-    //       </div>
-    //       <div className="project">
-    //         <img src={logo2} alt="Seokamela" id="logo" />
-    //         <div className="layer">
-    //           <h4>SnackTime Web app</h4>
-    //           <p>
-    //             A full stack mobile app. for food delivery,
-    //             Using React and PHP
-    //           </p>
-    //           <a href="/#">
-    //             <i class="fa-solid fa-up-right-from-square"></i>
-    //           </a>
-    //         </div>
-    //       </div>
-    //       <div className="project">
-    //         <img src={logo3} alt="Seokamela" id="logo" />
-    //         <div className="layer">
-    //           <h4>web app </h4>
-    //           <p>
-    //             A portfolio project using reactJS HTM and CSS.
-    //           </p>
-    //           <a href="/#">
-    //             <i class="fa-solid fa-up-right-from-square"></i>
-    //           </a>
-    //         </div>
-    //       </div>
-
-    //     </div>
-    //     {/* <a  className="btn-seemore" href="/#"> more </a>  */}
-    //   </div>
-    //   </div>
+  
     <div id="services-contants">
       <div className="projects">
         <div className="card">
